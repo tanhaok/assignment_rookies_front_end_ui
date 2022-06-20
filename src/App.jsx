cadc894e95ebad14905b5578501573476d2776cf
tanhao111 <haonguyentan2001@gmail.com>
2022-06-20 14:49:59 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Hello world</h1>
+        <h1>This is main</h1>
+      </div>
+    );
+  }
+}
+
+export default  App;
