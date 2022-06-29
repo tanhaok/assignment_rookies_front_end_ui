863935e68da9dc51a6c82bc5704804527cb393b3
tanhao111 <haonguyentan2001@gmail.com>
2022-06-29 14:31:52 +0700

    Remove Strict mode

--- src/index.js
+++ src/index.js
@@ -12,11 +12,7 @@ import "./asset/style/CustomSize.css";
 import "./asset/style/CustomStyleTag.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
