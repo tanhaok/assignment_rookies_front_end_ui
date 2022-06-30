eb9f748fe092a7a2b5448fd18df2b6e42adb2707
tanhao111 <haonguyentan2001@gmail.com>
2022-06-30 16:58:33 +0700

    add font and strict mode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import $ from "jquery";
 import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 // Custom style
-import "./asset/style/CustomSize.css";
-import "./asset/style/CustomStyleTag.css";
+import "./asset/style/CustomStyle.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
