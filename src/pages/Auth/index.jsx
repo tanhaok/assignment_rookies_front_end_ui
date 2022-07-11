1321ace02174b96c79b6f3eb5a0146da5661da3e
tanhao111 <haonguyentan2001@gmail.com>
2022-07-11 22:35:20 +0700

    update ui login, register

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, Navigate } from "react-router-dom";
 import { LogIn, Register } from "../../service/AccountService.js";
+import { TextField } from "@mui/material";
 
 const SignIn = () => {
   const {
@@ -25,19 +26,20 @@ const SignIn = () => {
       });
   };
   return (
-    <div className="log-in">
+    <div className="log-in d-flex flex-column justify-content-center align-items-center">
       {role === "USER" && <Navigate to={"/"}></Navigate>}
       {role === "ADMIN" && <Navigate to={"/admin"}></Navigate>}
       <div className="label">LOG IN</div>
-      <div className="form-log-in">
-        <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-log-in" style={{ width: "30rem" }}>
+        <form onSubmit={handleSubmit(onSubmit)} className=" d-flex flex-column">
           <label>Username</label>
           <input
             {...register("username", {
               required: true,
               maxLength: 20,
-              pattern: /^[A-Za-z]+$/i,
             })}
+            type="text"
+            style={{ marginBottom: ".5rem" }}
           />
           {errors?.firstName?.type === "required" && (
             <p>This field is required</p>
@@ -45,15 +47,14 @@ const SignIn = () => {
           {errors?.firstName?.type === "maxLength" && (
             <p>First name cannot exceed 20 characters</p>
           )}
-          {errors?.firstName?.type === "pattern" && (
-            <p>Alphabetical characters only</p>
-          )}
           <label>Password</label>
           <input
             {...register("password", {
               required: true,
               maxLength: 20,
             })}
+            type="password"
+            style={{ marginBottom: ".5rem" }}
           />
           {errors?.lastName?.type === "required" && (
             <p>This field is required</p>
@@ -61,8 +62,8 @@ const SignIn = () => {
           {errors?.lastName?.type === "maxLength" && (
             <p>First name cannot exceed 20 characters</p>
           )}
-          <input type="submit" value={"Log In"} />
-          <small>
+          <input type="submit" className="btn btn-primary" value={"Log In"} />
+          <small className="text-center">
             <Link to={"/register"}>Create New Account</Link>
           </small>
         </form>
@@ -96,30 +97,62 @@ const SignUp = () => {
       {role === "USER" && <Navigate to={"/"}></Navigate>}
       {role === "ADMIN" && <Navigate to={"/admin"}></Navigate>}
       <div className="register">
-        <div className="form">
+        <div
+          className="form w-100 d-flex justify-content-center"
+          style={{ marginTop: "2rem" }}
+        >
           {error && <div className="text-danger">{error}</div>}
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="d-flex flex-column w-25"
+          >
+            <TextField
               type="email"
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              style={{ marginBottom: "1rem" }}
               {...register("email", { required: true, maxLength: 30 })}
             />
-            <input
+            <TextField
+              type="text"
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              style={{ marginBottom: "1rem" }}
               {...register("username", { required: true, maxLength: 20 })}
             />
-            <input
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              style={{ marginBottom: "1rem" }}
               type="password"
               {...register("password", { required: true, maxLength: 20 })}
             />
-            <input
+            <TextField
+              id="outlined-basic"
+              label="Address"
+              variant="outlined"
+              style={{ marginBottom: "1rem" }}
+              type="text"
               {...register("address", { required: true, maxLength: 20 })}
             />
-            <input
+            <TextField
+              id="outlined-basic"
+              label="Phone Number"
+              variant="outlined"
+              style={{ marginBottom: "1rem" }}
               type="number"
               {...register("phone", {
                 required: true,
               })}
             />
-            <input type="submit" />
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value={"Register"}
+            />
           </form>
         </div>
       </div>
